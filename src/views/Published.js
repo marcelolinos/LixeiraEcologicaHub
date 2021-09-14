ab1a980d8f0c3b643c57ef86e98a75d4eecfaf4a
FelipePereiraOliveira <fellipeum@hotmail.com>
2021-09-14 16:29:13 -0300

    Att icons

--- src/views/Published.js
+++ src/views/Published.js
@@ -492,10 +492,25 @@ function Published(props){
                     <ModalHeader>Contato</ModalHeader>
                     <ModalBody >
                             
-                            <p>Nome: {publicacao.usuario.nome}</p>
-                            <p>Telefone: {publicacao.usuario.telefone}</p>
-                            <p>E-mail: {publicacao.usuario.email}</p>
-                            <p></p>
+                            <p>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-fill" viewBox="0 1 18 15">
+                                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                                </svg>
+                                Nome: {publicacao.usuario.nome}
+                            </p>
+                            <p>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 1 18 15">
+                                    <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+                                </svg>                                
+                                Telefone: {publicacao.usuario.telefone}
+                            </p>
+                            <p>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 1 18 15">
+                                    <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555zM0 4.697v7.104l5.803-3.558L0 4.697zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757zm3.436-.586L16 11.801V4.697l-5.803 3.546z"/>
+                                </svg>
+                                E-mail: {publicacao.usuario.email}
+                            </p>
+                            
                     </ModalBody>
                 </Modal>
             </div>
